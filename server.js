37e91fc93d8b64764c6a3ae541e47ece1d61fe3f
Wrong1234 <mahabur24cse@gmail.com>
2025-11-18 17:07:26 +0600

    cors setup

--- server.js
+++ server.js
@@ -32,9 +32,19 @@ const server = http.createServer(app);
 const io = initializeSocket(server);
 app.set('io', io);
 
+// Set up security middleware
 app.use(helmet());
+app.use(
+    cors({
+      origin: true,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    })
+  );
+
+
+
 app.use(compression());
-app.use(cors())
 app.post('/api/stripe/webhook', express.raw({ type: 'application/json' }), webhookController.handleWebhook);
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
