89c4c8a5a0ec701bcc9af2bb9b4deee570b90ccd
Wrong1234 <mahabur24cse@gmail.com>
2025-12-18 13:03:23 +0600

    fixups

--- models/Notification.js
+++ models/Notification.js
@@ -19,7 +19,7 @@ const notificationSchema = new mongoose.Schema({
   },
   type: {
     type: String,
-    enum: ['ride_request', 'ride_accepted', 'ride_completed', 'payment', 'promotion', 'system', 'rating_received', 'driver_request',  "driver_approval", "driver_rejected"],
+    enum: ['ride_request', 'ride_accepted', 'ride_completed', 'payment', 'promotion', 'system', 'rating_received', 'driver_request',  "driver_approval", "driver_rejected", "ride_cancelled"],
   },
   isRead: {
     type: Boolean,
